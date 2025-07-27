5e44ac560fb93bcbe295386674a86507a64a8962
Johannes Brock <info@johannes-brock.de>
2025-07-27 20:10:00 +0200

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,37 @@ export default defineConfig({
         './resources/views/**/*.blade.php',
         './src/**/*.php',
     ],
+
+    safelist: [
+        'grid-cols-2',
+        'grid-cols-3',
+        'grid-cols-4',
+        'grid-cols-5',
+        'grid-cols-6',
+        'grid-cols-7',
+        'grid-cols-8',
+        'grid-cols-9',
+        'grid-cols-10',
+        'grid-cols-11',
+        'grid-cols-12',
+        'bg-[#0298b0]',
+        'col-span-1',
+        'col-span-2',
+        'col-span-3',
+        'col-span-4',
+        'col-span-5',
+        'col-span-6',
+        'col-span-7',
+        'col-span-8',
+        'col-span-9',
+        'col-span-10',
+        'col-span-11',
+        'col-span-12',
+        'min-h-36',
+        'lg:ml-72',
+        'bg-green-60'
+    ],
+
     theme: {
         extend: {
             colors: {
@@ -52,4 +83,4 @@ export default defineConfig({
             strategy: 'class',
         }),
     ],
-}); 
+});
