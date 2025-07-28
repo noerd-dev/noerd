70b6b76766a324455cb4db26ca5ef16960b3e710
Johannes Brock <info@johannes-brock.de>
2025-07-28 19:40:00 +0200

    init

--- routes/noerd-routes.php
+++ routes/noerd-routes.php
@@ -2,6 +2,8 @@
 
 use Livewire\Volt\Volt;
 use Noerd\Noerd\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use Noerd\Noerd\Controllers\Auth\VerifyEmailController;
 
 Route::group(['middleware' => ['auth', 'verified', 'setup', 'web']], function (): void {
     Volt::route('setup', 'setup.users-table')->name('setup');
@@ -13,4 +15,28 @@ Route::group(['middleware' => ['auth', 'verified', 'setup', 'web']], function ()
 Route::group(['middleware' => ['auth', 'verified', 'web']], function (): void {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
     Route::view('profile', 'noerd::profile')->name('profile');
+});
+
+Route::middleware('guest')->group(function (): void {
+    Volt::route('login', 'auth.login')
+        ->name('login');
+
+    Volt::route('forgot-password', 'auth.forgot-password')
+        ->name('password.request');
+
+    Volt::route('reset-password/{token}', 'auth.reset-password')
+        ->name('password.reset');
+
+});
+
+Route::middleware('auth')->group(function (): void {
+    Volt::route('verify-email', 'auth.verify-email')
+        ->name('verification.notice');
+
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Volt::route('confirm-password', 'auth.confirm-password')
+        ->name('password.confirm');
 });
