bad16b30677679b07b8eb2d48b6057c696e7d04d
Johannes Sochor <info@johannes-sochor.de>
2025-08-01 09:23:19 +0200

    noerd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,55 @@
-module.exports = {
-  content: [
-      './resources/views/**/*.blade.php',
-  ],
-}
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
 
+require('dotenv').config();
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/views/**/*.blade.php',
+    ],
+
+    safelist: [
+        'grid-cols-2',
+        'grid-cols-3',
+        'grid-cols-4',
+        'grid-cols-5',
+        'grid-cols-6',
+        'grid-cols-7',
+        'grid-cols-8',
+        'grid-cols-9',
+        'grid-cols-10',
+        'grid-cols-11',
+        'grid-cols-12',
+        'bg-[#0298b0]',
+        'col-span-1',
+        'col-span-2',
+        'col-span-3',
+        'col-span-4',
+        'col-span-5',
+        'col-span-6',
+        'col-span-7',
+        'col-span-8',
+        'col-span-9',
+        'col-span-10',
+        'col-span-11',
+        'col-span-12',
+        'min-h-36',
+        'lg:ml-72',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            display: ['group-hover'],
+            colors: {
+                'brand-highlight': process.env.VITE_PRIMARY_COLOR || '#000',
+                'brand-bg': process.env.VITE_BG_COLOR || '#f9f9f9',
+            },
+        },
+    },
+
+    plugins: [forms, require('tailwind-scrollbar')],
+}
