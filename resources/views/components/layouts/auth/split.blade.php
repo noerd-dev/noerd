27c8dcece210700ae0e8e4cb11cd8cd8a3450ac0
Johannes Brock <info@johannes-brock.de>
2025-08-06 23:14:13 +0200

    noerd

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -6,24 +6,18 @@
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-                <div class="absolute inset-0 bg-neutral-900"></div>
+                @if(env('AUTH_BACKGROUND_IMAGE'))
+                    <div class="absolute inset-0 bg-cover bg-center bg-no-repeat" 
+                         style="background-image: url('{{ env('AUTH_BACKGROUND_IMAGE') }}');">
+                        <div class="absolute inset-0 bg-black/50"></div>
+                    </div>
+                @else
+                    <div class="absolute inset-0" style="background-color: {{ env('VITE_PRIMARY_COLOR', '#171717') }};"></div>
+                @endif
                 <a href="{{ route('noerd-home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
-                    <span class="flex h-10 w-10 items-center justify-center rounded-md">
-                        <x-noerd::app-logo-icon class="me-2 h-7 fill-current text-white" />
-                    </span>
+
                     {{ config('app.name', 'Laravel') }}
                 </a>
-
-                @php
-                    [$message, $author] = str(Illuminate\Foundation\Inspiring::quotes()->random())->explode('-');
-                @endphp
-
-                <div class="relative z-20 mt-auto">
-                    <blockquote class="space-y-2">
-                        <flux:heading size="lg">&ldquo;{{ trim($message) }}&rdquo;</flux:heading>
-                        <footer><flux:heading>{{ trim($author) }}</flux:heading></footer>
-                    </blockquote>
-                </div>
             </div>
             <div class="w-full lg:p-8">
                 <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
