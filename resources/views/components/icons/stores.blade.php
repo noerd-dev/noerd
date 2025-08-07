931d0e5c939694c64cf4bfa4d4511ffe9ac67ded
Johannes Brock <info@johannes-brock.de>
2025-08-07 22:14:42 +0200

    noerd

---
+++ resources/views/components/icons/stores.blade.php
@@ -0,0 +1,13 @@
+<div {{ $attributes->whereDoesntStartWith('class') }} {{ $attributes->merge(['class' => 'my-auto flex-1']) }}>
+    <svg class="nc-icon mx-auto" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 48 48"><title>
+            shop</title>
+        <g fill="#333" stroke-linecap="square" stroke-linejoin="miter" stroke-miterlimit="10">
+            <polyline points="20 44 20 32 28 32 28 44" fill="none" stroke="#333" stroke-width="2"
+                      stroke-linecap="butt"></polyline>
+            <polyline points="8 27 8 44 40 44 40 27" fill="none" stroke="#333" stroke-width="2"></polyline>
+            <path
+                d="M46,16a6.148,6.148,0,0,1-6.286,6,6.361,6.361,0,0,1-5.238-2.686,6.45,6.45,0,0,1-10.476,0,6.45,6.45,0,0,1-10.476,0A6.361,6.361,0,0,1,8.286,22,6.148,6.148,0,0,1,2,16L10,4H38Z"
+                fill="none" stroke="#333" stroke-width="2"></path>
+        </g>
+    </svg>
+</div>
