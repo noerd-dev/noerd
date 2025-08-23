9cc1072c8cca16bf75b538caebfcef0c16886639
Johannes Brock <info@johannes-brock.de>
2025-08-23 20:40:00 +0200

    noerd

--- src/Models/User.php
+++ src/Models/User.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Auth;
 use Noerd\Noerd\Database\Factories\UserFactory;
-use Noerd\Noerd\Models\Profile;
 use Nywerk\LegalRegister\Models\Standort;
 
 class User extends Authenticatable
