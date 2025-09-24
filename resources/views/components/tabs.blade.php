4b1d4385b6aea9369c5d0680d5baf7f8a3dc7e6b
Johannes Brock <info@johannes-brock.de>
2025-09-24 18:40:00 +0200

    update tabs margin

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,5 +1,5 @@
-<div class="py-6">
-    <div class="border-b border-gray-300 flex">
+<div class="py-6 w-full">
+    <div class="border-b border-gray-300 flex w-full">
         <nav class="inline-block" aria-label="Tabs">
             {{$slot}}
         </nav>
