1b3bf532f08943a8fac91ffa1ec8f3794a4aee92
Johannes Brock <info@johannes-brock.de>
2025-12-08 21:06:07 +0100

    update generic tabs

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -5,10 +5,26 @@
         <div class="border-b border-gray-300 flex w-full">
             <nav class="inline-block" aria-label="Tabs">
                 @foreach($layout['tabs'] as $tab)
-                    @if(!($tab['requiresId'] ?? false) || $modelId)
-                        <x-noerd::tab :tabNumber="$tab['number']">
-                            {{ __($tab['label']) }}
-                        </x-noerd::tab>
+                    @php
+                        $showTab = true;
+                        if (isset($tab['requiresId']) && $tab['requiresId'] && !$modelId) {
+                            $showTab = false;
+                        }
+                        if (isset($tab['permission'])) {
+                            $permissionModel = $tab['permissionModel'] ?? null;
+                            $showTab = $showTab && Gate::allows($tab['permission'], $permissionModel);
+                        }
+                    @endphp
+                    @if($showTab)
+                        @if(isset($tab['route']))
+                            <x-noerd::tab :route="$tab['route']" :active="request()->routeIs($tab['route'])">
+                                {{ __($tab['label']) }}
+                            </x-noerd::tab>
+                        @else
+                            <x-noerd::tab :tabNumber="$tab['number']">
+                                {{ __($tab['label']) }}
+                            </x-noerd::tab>
+                        @endif
                     @endif
                 @endforeach
             </nav>
