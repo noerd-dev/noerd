aa12e610b106792334d34467bf532343eea491e0
Johannes Brock <info@johannes-brock.de>
2025-12-14 22:56:03 +0100

    add success message when new tenant created

--- resources/views/livewire/setup/create-new-tenant.blade.php
+++ resources/views/livewire/setup/create-new-tenant.blade.php
@@ -7,6 +7,7 @@ use Livewire\Volt\Component;
 
 new class extends Component {
     public string $name = '';
+    public bool $showSuccess = false;
 
     public function createTenant()
     {
@@ -56,7 +57,7 @@ new class extends Component {
         $user->selected_tenant_id = $tenant->id;
         $user->save();
 
-        return $this->redirect('/');
+        $this->showSuccess = true;
     }
 }; ?>
 
@@ -83,4 +84,24 @@ new class extends Component {
             <x-noerd::primary-button>{{ __('Neuen Mandanten erstellen') }}</x-noerd::primary-button>
         </div>
     </form>
+
+    @if($showSuccess)
+        <div class="rounded-md bg-green-50 p-4 mt-6"
+             x-init="setTimeout(() => { window.location.href = '/' }, 2000)">
+            <div class="flex">
+                <div class="shrink-0">
+                    <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </div>
+                <div class="ml-3">
+                    <p class="text-sm font-medium text-green-800">
+                        {{ __('Tenant successfully created. Redirecting...') }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    @endif
 </section>
