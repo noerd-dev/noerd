2e1a5370c790d91b572dd23a54bb858ea7f87419
Johannes Brock <info@johannes-brock.de>
2025-12-22 20:14:34 +0100

    update auth and buttons

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -10,7 +10,7 @@
 @endphp
 
 <div class="mt-auto flex h-full">
-    <x-noerd::primary-button wire:click="{{ $name }}" class="mt-auto !h-[40px]">
+    <x-noerd::buttons.primary wire:click="{{ $name }}" class="mt-auto !h-[40px]">
         {{ $label }}
-    </x-noerd::primary-button>
+    </x-noerd::buttons.primary>
 </div>
