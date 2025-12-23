232c93c534b2c53c7b962545e0d28882fae1bbeb
Johannes Brock <info@johannes-brock.de>
2025-12-23 12:47:46 +0100

    remove unused components

--- resources/views/components/buttons/black.blade.php
+++
@@ -1,8 +0,0 @@
-<button type="button"
-    {{ $attributes->whereDoesntStartWith('class') }}
-    {{ $attributes->merge(['class' => 'my-auto gap-2 h-9 inline-flex items-center text-white rounded-sm bg-slate-900 px-4 py-1.5 text-sm shadow-xs hover:bg-slate-800']) }}>
-    @isset($icon)
-        <x-dynamic-component class="w-5 h-5" :component="$icon"/>
-    @endisset
-    {{ $slot }}
-</button>
--- resources/views/components/buttons/function.blade.php
+++
@@ -1,7 +0,0 @@
-<button type="button" {{ $attributes->whereDoesntStartWith('class') }}
-    {{ $attributes->merge(['class' => 'inline-flex mr-1 shadow-sm items-center rounded-sm border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 hover:bg-gray-50 focus:outline-hidden focus:ring-2 focus:ring-black focus:ring-offset-2']) }}>
-    @isset($icon)
-        <x-dynamic-component class="w-5 h-5 pr-2" :component="$icon"/>
-    @endisset
-    <span>{{ $slot }}</span>
-</button>
