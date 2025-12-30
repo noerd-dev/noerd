5c3cc95467653cd4c8a80c7e3b167e9a54254fc3
Johannes Brock <info@johannes-brock.de>
2025-12-30 20:40:00 +0100

    responsive design updates

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false, 'startField' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 disabled:opacity-50 block w-full !py-1.5 px-3 rounded-xs border border-gray-300 shadow-xs focus:outline-none focus:ring-2 focus:ring-brand-border focus:ring-offset-2 sm:text-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 disabled:opacity-50 block w-full !py-1.5 px-3 rounded-xs border border-gray-300 shadow-xs focus:outline-none focus:ring-2 focus:ring-brand-border focus:ring-offset-2 text-sm']) !!}>
