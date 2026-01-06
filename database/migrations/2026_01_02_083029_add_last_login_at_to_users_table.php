56091826943a38ebb7392920d9ac905e60eee6d8
Johannes Brock <info@johannes-brock.de>
2026-01-06 18:10:00 +0100

    pint

--- database/migrations/2026_01_02_083029_add_last_login_at_to_users_table.php
+++ database/migrations/2026_01_02_083029_add_last_login_at_to_users_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table): void {
             $table->timestamp('last_login_at')->nullable()->after('remember_token');
         });
     }
@@ -21,7 +20,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table): void {
             $table->dropColumn('last_login_at');
         });
     }
