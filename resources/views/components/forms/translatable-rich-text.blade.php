c2e3f35a2a142b671bcd547c281fb38d29c832f1
Johannes Brock <info@johannes-brock.de>
2026-01-09 20:48:29 +0100

    wip

--- resources/views/components/forms/translatable-rich-text.blade.php
+++ resources/views/components/forms/translatable-rich-text.blade.php
@@ -7,14 +7,19 @@
 @php
     $name = $field['name'] ?? $name;
     $label = $field['label'] ?? $label;
+    $baseKey = str_replace('model.', '', $name);
+    $selectedLang = session('selectedLanguage') ?? 'de';
+    // Access model via $this (Livewire component) since $model may not be in scope
+    $modelData = $this->model ?? $model ?? [];
+    $contentValue = $modelData[$baseKey][$selectedLang] ?? '';
 @endphp
 
-<div wire:key="{{ $name . (session('selectedLanguage') ?? 'de') }}" {{ $attributes->merge(['class' => '']) }}>
+<div wire:key="{{ $name . $selectedLang }}" {{ $attributes->merge(['class' => '']) }}>
     <x-noerd::input-label for="{{ $name }}" :value="__($label)"/>
 
     <x-noerd::forms.tiptap
-        :field="$name . '.' . (session('selectedLanguage') ?? 'de')"
-        :content="$model[str_replace('model.', '', $name)][session('selectedLanguage')] ?? ''"
+        :field="$name . '.' . $selectedLang"
+        :content="$contentValue"
     />
 
     <x-noerd::input-error :messages="$errors->get($name)" class="mt-2"/>
