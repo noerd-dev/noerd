2900d44a544f2614683923f3c5c456a421027c80
Johannes Brock <info@johannes-brock.de>
2026-01-15 20:33:39 +0100

    fix updated at

--- src/Models/Tenant.php
+++ src/Models/Tenant.php
@@ -20,7 +20,7 @@ class Tenant extends Authenticatable
         'frontendSession',
     ];
 
-    protected $guarded = [];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 
     protected $hidden = [
         'password',
