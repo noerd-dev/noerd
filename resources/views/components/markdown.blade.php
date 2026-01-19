0e55163f797bb1d8e825e649082a5a35df543a55
Johannes Brock <info@johannes-brock.de>
2026-01-19 18:40:00 +0100

    fix empty line markdown

--- resources/views/components/markdown.blade.php
+++ resources/views/components/markdown.blade.php
@@ -1,8 +1,14 @@
 @props(['content' => ''])
 
 @php
-    $html = \Illuminate\Support\Str::markdown($content ?? '', [
-        'html_input' => 'strip',
+    $processedContent = $content ?? '';
+
+    // Preserve multiple consecutive empty lines by converting them to <br> tags
+    // 3+ newlines = 2+ empty lines, convert extra empty lines to <br>
+    $processedContent = preg_replace('/\n{3,}/', "\n\n<br>\n\n", $processedContent);
+
+    $html = \Illuminate\Support\Str::markdown($processedContent, [
+        'html_input' => 'allow',
         'allow_unsafe_links' => false,
     ]);
 @endphp
