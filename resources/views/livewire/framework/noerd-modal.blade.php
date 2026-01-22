dfb451420970f95063a5532111df7eb584eb6750
Johannes Brock <info@johannes-brock.de>
2026-01-22 22:25:20 +0100

    fix modal

--- resources/views/livewire/framework/noerd-modal.blade.php
+++ resources/views/livewire/framework/noerd-modal.blade.php
@@ -3,8 +3,9 @@
 use Livewire\Volt\Component;
 use Illuminate\Support\Str;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Isolate;
 
-new class extends Component {
+new #[Isolate] class extends Component {
 
     public array $modals = [];
 
@@ -21,16 +22,8 @@ new class extends Component {
         $modal['show'] = true;
         $modal['topModal'] = false;
         $modal['source'] = $source;
-        $modal['key'] = md5(serialize($arguments));
-
-        // DEBUG
-        \Log::debug('[noerd-modal Debug] bootModal', [
-            'component' => $modalComponent,
-            'arguments' => $arguments,
-            'serialized' => serialize($arguments),
-            'key' => $modal['key'],
-            'existing_modal_keys' => array_keys($this->modals),
-        ]);
+        // Eindeutigerer Key mit Timestamp für Uniqueness
+        $modal['key'] = md5(serialize($arguments) . microtime());
 
         $iteration = 1;
         foreach ($this->modals as $checkModal) {
@@ -45,14 +38,14 @@ new class extends Component {
         $this->markTopModal();
     }
 
-    public function downModal(string $componentName, ?string $source, ?string $modalKey): void // by ESC f.e.
+    public function downModal(string $componentName, ?string $source, ?string $modalKey): void
     {
         $this->dispatch('close-modal-' . $componentName, $source, $modalKey);
-        $this->dispatch('refreshList-' . $source); // Reload the table, if it is a table component
+        $this->dispatch('refreshList-' . $source);
     }
 
     #[On('downModal2')]
-    public function downModal2(string $componentName, ?string $source, ?string $modalKey): void // by ESC f.e.
+    public function downModal2(string $componentName, ?string $source, ?string $modalKey): void
     {
         $modals = $this->modals;
         foreach ($modals as $modal) {
@@ -61,10 +54,9 @@ new class extends Component {
             }
         }
 
-        $this->dispatch('refreshList-' . $source); // Reload the table, if it is a table component
+        $this->dispatch('refreshList-' . $source);
         $this->markTopModal();
 
-        // Check if no modals are open and reset modalOpen flag
         $hasOpenModal = false;
         foreach ($this->modals as $modal) {
             if ($modal['show'] === true) {
@@ -74,7 +66,6 @@ new class extends Component {
         }
 
         if (!$hasOpenModal) {
-            // This re-enables keyboard control in tables.
             $this->dispatch('modal-closed-global');
         }
     }
@@ -112,28 +103,29 @@ new class extends Component {
 <div x-data="{selectedRow: 0, isDragging: false, isLoading: false}">
     @isset($modals)
         @foreach($modals as $key => $modal)
-            <div x-data="{ show: true }" wire:key="modal-wrapper-{{$modal['key']}}"
-                 @if($modal['show'] && $modal['topModal'])
-                     x-init="$store.app.modalOpen = true"
-                 @close-modal-{{$modal['componentName']}}.prevent.stop="$wire.downModal('{{$modal['componentName']}}', '{{$modal['source']}}', '{{$modal['key']}}')"
-                 @keydown.escape.window.prevent.stop="$wire.downModal('{{$modal['componentName']}}', '{{$modal['source']}}', '{{$modal['key']}}')"
-                @endif
-            >
-                <div x-show="show">
-                    <x-noerd::modal>
-                        <x-noerd::modal.panel :ml="$modal['arguments']['ml'] ?? ''"
-                                              :iteration="$modal['iteration']"
-                                              :source="$modal['source']"
-                                              :modalKey="$modal['key']"
-                                              :modal="$modal['componentName']">
-                            <div wire:key="modal-content-{{$modal['key']}}">
-                                @livewire($modal['componentName'], $modal['arguments'], key($modal['key']))
-                            </div>
-                        </x-noerd::modal.panel>
-                    </x-noerd::modal>
+            @teleport('body')
+                <div x-data="{ show: true }" wire:key="modal-wrapper-{{$modal['key']}}"
+                     @if($modal['show'] && $modal['topModal'])
+                         x-init="$store.app.modalOpen = true"
+                     @close-modal-{{$modal['componentName']}}.prevent.stop="$wire.downModal('{{$modal['componentName']}}', '{{$modal['source']}}', '{{$modal['key']}}')"
+                     @keydown.escape.window.prevent.stop="$wire.downModal('{{$modal['componentName']}}', '{{$modal['source']}}', '{{$modal['key']}}')"
+                    @endif
+                >
+                    <div x-show="show">
+                        <x-noerd::modal>
+                            <x-noerd::modal.panel :ml="$modal['arguments']['ml'] ?? ''"
+                                                  :iteration="$modal['iteration']"
+                                                  :source="$modal['source']"
+                                                  :modalKey="$modal['key']"
+                                                  :modal="$modal['componentName']">
+                                <div wire:ignore>
+                                    @livewire($modal['componentName'], $modal['arguments'], key($modal['key']))
+                                </div>
+                            </x-noerd::modal.panel>
+                        </x-noerd::modal>
+                    </div>
                 </div>
-            </div>
+            @endteleport
         @endforeach
     @endisset
 </div>
-
