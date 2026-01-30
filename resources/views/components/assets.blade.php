ebbd47b98a55caa31739d99b921b5df6e35d254e
Johannes Brock <info@johannes-brock.de>
2026-01-30 10:16:05 +0100

    update assets path

--- resources/views/components/assets.blade.php
+++ resources/views/components/assets.blade.php
@@ -1,16 +1,1 @@
-@php
-    $manifestPath = public_path('vendor/noerd/manifest.json');
-    $hotFilePath = public_path('vendor/noerd/hot');
-@endphp
-
-@if(file_exists($manifestPath) || file_exists($hotFilePath))
-    @php
-        $vite = clone app(\Illuminate\Foundation\Vite::class);
-    @endphp
-
-    {{
-        $vite->useHotFile($hotFilePath)
-            ->useBuildDirectory('vendor/noerd')
-            ->withEntryPoints(['resources/js/noerd.js'])
-    }}
-@endif
+@vite(['app-modules/noerd/resources/js/noerd.js'])
