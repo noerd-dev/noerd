3d8d67504adf638044f5868c273b4a1abd7aae77
Johannes Brock <info@johannes-brock.de>
2026-02-07 17:18:49 +0100

    fix

--- routes/noerd-routes.php
+++ routes/noerd-routes.php
@@ -6,15 +6,15 @@ use Noerd\Controllers\DashboardController;
 Route::group(['middleware' => ['auth', 'verified', 'setup', 'web']], function (): void {
     Route::livewire('setup', 'setup.users-list')->name('setup');
     Route::livewire('users', 'setup.users-list')->name('users');
-    Route::livewire('user/{model}', 'user-detail')->name('user.detail');
+    Route::livewire('user/{modelId}', 'user-detail')->name('user.detail');
     Route::livewire('user-roles', 'setup.user-roles-list')->name('user-roles');
-    Route::livewire('user-role/{model}', 'user-role-detail')->name('user-role.detail');
+    Route::livewire('user-role/{modelId}', 'user-role-detail')->name('user-role.detail');
     Route::livewire('tenant', 'setup.tenant-detail')->name('tenant');
     Route::livewire('models', 'models-list')->name('models');
     Route::livewire('setup-collections', 'setup-collections-list')->name('setup-collections');
-    Route::livewire('setup-collection/{model}', 'setup-collection-detail')->name('setup-collection.detail');
+    Route::livewire('setup-collection/{modelId}', 'setup-collection-detail')->name('setup-collection.detail');
     Route::livewire('setup-languages', 'setup-languages-list')->name('setup-languages');
-    Route::livewire('setup-language/{model}', 'setup-language-detail')->name('setup-language.detail');
+    Route::livewire('setup-language/{modelId}', 'setup-language-detail')->name('setup-language.detail');
 });
 
 Route::group(['middleware' => ['auth', 'web']], function (): void {
