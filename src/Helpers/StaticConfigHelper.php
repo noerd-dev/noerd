6217522a9b23fee95aa07e166bdebf7c07942d93
johbrock <info@johannes-brock.de>
2026-05-06 20:30:08 +0200

    Remove booking-members fallback to booking app-configs

--- src/Helpers/StaticConfigHelper.php
+++ src/Helpers/StaticConfigHelper.php
@@ -121,12 +121,6 @@ class StaticConfigHelper
             $allowedFolders[] = mb_strtolower($appName);
         }
 
-        // booking-members is a superset of booking; allow access to booking app-configs
-        if (in_array('booking-members', $allowedFolders, true)
-            && ! in_array('booking', $allowedFolders, true)) {
-            $allowedFolders[] = 'booking';
-        }
-
         return $allowedFolders;
     }
 
